da2a49b4feac11bd81a41073e7a18581b3bf9e44
Janzen Brewer <jahabrewer@gmail.com>
2011-06-21 22:35:44 -0400

    added validation for absence dates

--- models/absence.php
+++ models/absence.php
@@ -3,6 +3,19 @@ class Absence extends AppModel {
 	var $name = 'Absence';
 	var $displayField = 'absentee_id';
 
+	var $validate = array(
+		'start' => array(
+			'startBeforeEnd' => array(
+				'rule' => array('startBeforeEnd', 'end'),
+				'message' => 'The absence must start before it ends'
+			),
+			'futureDate' => array(
+				'rule' => array('futureDate', 'start'),
+				'message' => 'The absence must begin in the future'
+			)
+		)
+	);
+
 	var $belongsTo = array(
 		'Absentee' => array(
 			'className' => 'User',
@@ -17,4 +30,22 @@ class Absence extends AppModel {
 			'foreignKey' => 'school_id',
 		)
 	);
+
+	function startBeforeEnd($field=array(), $compare_field) {
+		foreach($field as $key => $value) {
+			$v1 = $value;
+			$v2 = $this->data[$this->name][$compare_field];
+			if ($v1 >= $v2) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+	function futureDate($data, $field){
+		if (strtotime($data[$field]) <= time()){
+			return FALSE;
+		}
+		return TRUE;
+	} 
 }
